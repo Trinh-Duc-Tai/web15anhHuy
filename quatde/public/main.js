0abffa570db7740aa457ebd36015d33054d5f544
Trinh Duc Tai <trinhductai6869@gmail.com>
2018-10-10 22:42:55 +0700

    het buoi 6

--- quatde/public/main.js
+++ quatde/public/main.js
@@ -1,13 +1,49 @@
-console.log("Hello");
+// axios.get("http://localhost:3000/randomquestion")
+// .then(function (response) {
+//     if(response.data) {
+// 		document.getElementById("questionContent").innerText = response.data.questionContent;
+// 	}
+// })
+// .catch(function (error) {
+// 	console.log(error);
+// });
 
-const maxLength = 200;
-const questionContentElem = document.getElementById('idQuestion');
-const remainCharElem = document.getElementById('remain');
+function getRandomQuestion() {
+	$.ajax({
+		url: "http://localhost:3000/randomquestion",
+		type: "GET",
+		success: function(response) {
+			if(response) {
+				$("#questionContent").text(response.questionContent);
+				$(".answer_btn").data("questionid", response.id);
+			}
+		},
+		error: function(err) {
+			console.log(err);
+		}
+	});
+}
 
-questionContentElem.addEventListener(
-    'input',
-    function(){
-    var remainChar = maxLength - questionContentElem.value.length;
-    remainCharElem.innerHTML = remainChar;
-    }
-);
+getRandomQuestion();
+
+$("#otherQuestion").on("click", function() {
+	getRandomQuestion();
+});
+
+$(".answer_btn").on("click", function() {
+	console.log($(this).data())
+	$.ajax({
+		url: "http://localhost:3000/answer",
+		type: "POST",
+		data: $(this).data(),
+		success: function(response) {
+			if(response.success) {
+				window.location.href = "/";
+			}
+		},
+		error: function(err) {
+			console.log(err);
+			window.location.href = "https://google.com/search?q=site:stackoverflow.com why my code didn't run";
+		}
+	})
+});
--- quatde/server.js
+++ quatde/server.js
@@ -1,39 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const fs = require('fs');
+const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.sendfile(__dirname+'/public/answer.html');
-});
-app.get('/ask',(req,res)=>{
-    res.sendfile(__dirname+'/public/ask.html');
-});
-app.get('/answer',(req,res)=>{
-    res.sendfile(__dirname+'/public/answer.html');
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/answer.html');
 });
 
-app.post('/createquestion',(req,res)=>{
-    console.log(req.body);
-    let questionList = JSON.parse(fs.readFileSync('./question.json'));
-    const newQuestion = {
-        id: questionList.length, //0,1,2,3
-        questionContent: req.body.questionContent,
-        yes:0,
-        no:0
-    }
-    questionList.push(newQuestion);
-    fs.writeFileSync('./question.json',JSON.stringify(questionList));
-    res.redirect('/answer');
+app.get('/ask', (req, res) => {
+	res.sendFile(__dirname + '/public/ask.html');
+});
+
+app.get('/answer', (req, res) => {
+	res.sendFile(__dirname + '/public/answer.html');
+});
+
+app.post('/createquestion', (req, res) => {
+	let questionList = JSON.parse(fs.readFileSync('./questions.json'));
+
+	const newQuestion = {
+		id: questionList.length,
+		questionContent: req.body.questionContent,
+		yes: 0,
+		no: 0
+	};
+
+	questionList.push(newQuestion);
+
+	fs.writeFileSync('./questions.json', JSON.stringify(questionList));
+
+	res.redirect('/answer');
+});
+
+app.get('/randomquestion', (req, res) => {
+	let questionList = JSON.parse(fs.readFileSync('./questions.json'));
+
+	if(questionList.length > 0) {
+		let randomIndex = Math.floor(Math.random()*questionList.length);
+		let questionRandom = questionList[randomIndex];
+		res.send(questionRandom);
+	}
+});
+
+app.post('/answer', (req, res) => {
+	const { questionid, answer } = req.body;
+	// const questionid = req.body.questionid;
+	// const answer = req.body.answer;
+	let questionList = JSON.parse(fs.readFileSync('./questions.json'));
+	questionList[questionid][answer] += 1;
+	fs.writeFileSync('./questions.json', JSON.stringify(questionList));
+	res.send({ success: 1 });
 });
 
 app.use(express.static('public'));
 
-
-
-app.listen(6969,(err)=>{
-    if(err) console.log(err)
-    else console.log('Server is listening at port 6969');
+app.listen(3000, (err) => {
+	if(err) console.log(err)
+	else console.log('Server is listening at port 3000');
 });
